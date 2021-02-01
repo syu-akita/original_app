10d6e7c46a81911b86bfdbf2f4d41ca872e168b1
syu-akita <syu.akita.0612@gmail.com>
2021-02-01 22:33:48 +0900

    メッセージコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'rooms#index'
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
